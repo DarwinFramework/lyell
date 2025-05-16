11de25966d75ecf44430631162edf331f9639607
helight <contact@helight.dev>
2025-05-16 18:47:16 +0200

    feat: improve type capture methods and add SyntheticTypeCapture class

--- packages/lyell/lib/src/lyell_base.dart
+++ packages/lyell/lib/src/lyell_base.dart
@@ -8,27 +8,41 @@ abstract class TypeCapture<T> {
   const TypeCapture();
 
   TypeCapture get nullable => TypeToken<T?>();
+
   bool get isNullable => null is T;
+
   Type get typeArgument => T;
+
   Type get deriveList => List<T>;
+
   Type get deriveSet => Set<T>;
+
   Type get deriveIterable => Iterable<T>;
+
   Type get deriveFuture => Future<T>;
+
   Type get deriveFutureOr => FutureOr<T>;
+
   Type get deriveStream => Stream<T>;
 
   List<T> castList(List<dynamic> list) => list.cast<T>();
+
   Set<T> castSet(Set<dynamic> set) => set.cast<T>();
+
   Iterable<T> castIterable(Iterable<T> iterable) => iterable.cast<T>();
+
   bool isAssignable(dynamic object) => object is T;
+
   RETURN consumeType<RETURN>(RETURN Function<_>() func) => func<T>();
-  RETURN consumeTypeArg<RETURN,ARG>(RETURN Function<_>(ARG) func, ARG arg) => func<T>(arg);
+
+  RETURN consumeTypeArg<RETURN, ARG>(RETURN Function<_>(ARG) func, ARG arg) =>
+      func<T>(arg);
 }
 
 /// Unsafe type capture that only provides the passed in type and otherwise
 /// behaves like a dynamic type capture.
-class UnsafeRuntimeTypeCapture extends TypeCapture<dynamic> implements TypeTree<dynamic> {
-
+class UnsafeRuntimeTypeCapture extends TypeCapture<dynamic>
+    implements TypeTree<dynamic> {
   final Type type;
 
   @override
@@ -48,7 +62,7 @@ class UnsafeRuntimeTypeCapture extends TypeCapture<dynamic> implements TypeTree<
   @override
   bool isAssignable(dynamic object) => object.runtimeType == type;
 
-   @override
+  @override
   Iterable castIterable(Iterable iterable) => iterable;
 
   @override
@@ -61,40 +75,86 @@ class UnsafeRuntimeTypeCapture extends TypeCapture<dynamic> implements TypeTree<
   TypeCapture get base => this;
 }
 
-/// Mixin for adding the [TypeCapture] interface to classes.
-mixin TypeCaptureMixin<T> implements TypeCapture<T> {
+final class SyntheticTypeCapture extends QualifiedTypeTree<dynamic, dynamic> {
+  final String identity;
+  @override
+  final List<TypeTree> arguments;
+
+  const SyntheticTypeCapture(
+    this.identity, {
+    this.arguments = const [],
+  });
 
   @override
+  TypeCapture get base => TypeToken<dynamic>();
+
+  @override
+  bool operator ==(Object other) {
+    return other is SyntheticTypeCapture &&
+        other.identity == identity &&
+        const ListEquality().equals(arguments, other.arguments);
+  }
+
+  @override
+  int get hashCode => identity.hashCode ^ const ListEquality().hash(arguments);
+
+  @override
+  String toString() {
+    if (arguments.isEmpty) {
+      return "#$identity";
+    } else {
+      return "#$identity<${arguments.join(", ")}>";
+    }
+  }
+}
+
+/// Mixin for adding the [TypeCapture] interface to classes.
+mixin TypeCaptureMixin<T> implements TypeCapture<T> {
+  @override
   TypeCapture get nullable => TypeToken<T?>();
+
   @override
   bool get isNullable => null is T;
+
   @override
   Type get typeArgument => T;
+
   @override
   Type get deriveList => List<T>;
+
   @override
   Type get deriveSet => Set<T>;
+
   @override
   Type get deriveIterable => Iterable<T>;
+
   @override
   Type get deriveFuture => Future<T>;
+
   @override
   Type get deriveFutureOr => FutureOr<T>;
+
   @override
   Type get deriveStream => Stream<T>;
 
   @override
   List<T> castList(List<dynamic> list) => list.cast<T>();
+
   @override
   Set<T> castSet(Set<dynamic> set) => set.cast<T>();
+
   @override
   Iterable<T> castIterable(Iterable<T> iterable) => iterable.cast<T>();
+
   @override
   bool isAssignable(dynamic object) => object is T;
+
   @override
   RETURN consumeType<RETURN>(RETURN Function<_>() func) => func<T>();
+
   @override
-  RETURN consumeTypeArg<RETURN,ARG>(RETURN Function<_>(ARG) func, ARG arg) => func<T>(arg);
+  RETURN consumeTypeArg<RETURN, ARG>(RETURN Function<_>(ARG) func, ARG arg) =>
+      func<T>(arg);
 }
 
 /// Defines the item type of the implementing class.
@@ -108,13 +168,30 @@ abstract class CascadeItemType<T> extends TypeToken<T> {
 /// Default implementation for a [TypeCapture] of type [T].
 class TypeToken<T> extends TypeCapture<T> {
   const TypeToken();
+
+  @override
+  bool operator ==(Object other) {
+    if (other is TypeCapture) {
+      return other.typeArgument == typeArgument &&
+          other is! SyntheticTypeCapture;
+    }
+    return false;
+  }
+
+  @override
+  int get hashCode => typeArgument.hashCode;
+
+  @override
+  String toString() => "*$T";
 }
 
 /// Double type capture token for associated item types.
 /// Mainly useful for serialization or type mapping.
 class ItemAssociatedTypeToken<SOURCE, ITEM> {
   const ItemAssociatedTypeToken();
+
   TypeToken<SOURCE> get sourceType => TypeToken<SOURCE>();
+
   TypeToken<ITEM> get itemType => TypeToken<ITEM>();
 }
 
